52f968499d63ffd2a2a907f74481c545e58b77f7
houyuqin <1738519809@qq.com>
2020-02-27 14:50:12 +0800

    add eslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,16 @@
+/* global module: true */
 module.exports = function (grunt) {
   grunt.initConfig({
-    csslint: {
+    eslint: {
       options: {
-        csslintrc: '.csslintrc'
+        eslintrc: './.eslintrc.json'
       },
-      src: ['*.css']
+      target: ['*.js']
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-eslint');
 
-  grunt.registerTask('default', ['csslint']);
+  grunt.registerTask('default', ['eslint']);
+
 };
--- rectangle.js
+++ rectangle.js
@@ -1,23 +1,24 @@
+/* global $: true */
 $(function(){
-    /*calc button click event*/
-    //get dom elem
-    var $width = $("#width"),
-        $height = $("#height"),
-        $btnCal = $("#calculate"),
-        $perimeter = $("#perimeter"),
-        $area = $("#area")
+  /*calc button click event*/
+  //get dom elem
+  var $width = $('#width'),
+      $height = $('#height'),
+      $btnCal = $('#calculate'),
+      $perimeter = $('#perimeter'),
+      $area = $('#area');
 
-    $btnCal.click(function(){
-      //get value
-      var w = Number($width.val()),
-          h = Number($height.val());
+  $btnCal.click(function(){
+    //get value
+    var w = Number($width.val()),
+        h = Number($height.val());
 
-      //calculate
-      var p = 2*(w+h),
-          a = w*h;
+    //calculate
+    var p = 2*(w+h),
+        a = w*h;
 
-      //output
-      $perimeter.val(p.toFixed(2));
-      $area.val(a.tofix(2));
-    }); 
-})
+    //output
+    $perimeter.val(p.toFixed(2));
+    $area.val(a.tofix(2));
+  }); 
+});
