e23ff5219643b0003826744dd0f659a31e8d84d1
houyuqin <1738519809@qq.com>
2020-03-03 15:19:42 +0800

    uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,11 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    cssmin: {  
-      'rectangle.min.css': 'rectangle.css'
+    uglify: {
+          ' rectangle.min.js': 'rectangle.js'
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['cssmin']); 
-};
+  grunt.registerTask('default', ['uglify']);
+}; 
