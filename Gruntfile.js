3b1dba497ce3ad863cc733223031fcbdb463c41f
houyuqin <1738519809@qq.com>
2020-03-04 08:17:41 +0800

    imagesmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,18 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    uglify: {
-          ' rectangle.min.js': 'rectangle.js'
-    }
+        imagemin: {                               
+          files: {
+                    expand: true,
+          src: ['images/*.{png,jpg,gif}'],
+                                 dest: 'dist/'
+                                       
+          }
+              }
+                
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-  grunt.registerTask('default', ['uglify']);
-}; 
+      grunt.registerTask('default', ['imagemin']);    
+
+};
