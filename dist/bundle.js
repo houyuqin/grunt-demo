31bebcac8410f4713334b51e166170bcba66886f
houyuqin <1738519809@qq.com>
2020-03-04 08:42:12 +0800

    cancat2

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,22 +1,17 @@
-$(function(){
-    /*calc button click event*/
-    //get dom elem
-    var $width = $("#width"),
-        $height = $("#height"),
-        $btnCal = $("#calculate"),
-        $perimeter = $("#perimeter"),
-        $area = $("#area")
+$function() {
+  var $width = $('#width'),
+      $height = $('#height'),
+      $btnCal = $('#calculate'),
+      $perimeter = $('#perimeter'),
+      $area = $('#area');
 
-    $btnCal.click(function(){
-      //get value
-      var w = Number($width.val()),
-          h = Number($height.val());
+  $btnCal.click(function(){
+    var w = $width.val(),
+        h = $height.val();
 
-      //calculate
-      var r = new Rectangle(w,h);
+    var r = new Rectangle(w, h);
 
-      //output
-      $perimeter.val(p.toFixed(2));
-      $area.val(a.toFixed(2));
-    }); 
-})
+    $perimeter.val(r.perimeter());
+    $area.val(r.area());
+  });
+});
